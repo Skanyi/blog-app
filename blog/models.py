007381a8e733b7be5492446587b467449d0f52c2
pythonGeek <stephen.kanyi@andela.com>
2017-08-16 09:55:14 +0300

    Adds the post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+class Likes(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    likes_count = models.IntegerField(default=0)
