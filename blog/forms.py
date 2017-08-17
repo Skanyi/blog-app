fe244aac03c57e6255f84f3d8f0aab4a88a11e3c
andela-skanyi <stephenkanyi7@gmail.com>
2017-08-17 13:58:07 +0300

    adds send email task

---
+++ blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class EmailForm(forms.Form):
+    name=forms.CharField(label='Name')
+    email=forms.EmailField(label='Email')
+    message=forms.CharField(label=Message, widget=forms.TextArea(attrs={'rows': 5}))
+    honeypot = forms.CharField(widget=forms.HiddenInput(), required=False)
--- blog/tasks.py
+++ blog/tasks.py
@@ -1,8 +1,19 @@
-# create tasks here 
+# create tasks here that send an email every 24 hours
+# for testing purposes we will use every minute and when it works change to 24 hours
 
 from __future__ import absolute_import
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
-@shared_task
-def add(x, y):
-    return x + y
+from .emails import send_feedback_email
+
+logger = get_task_logger(__name__)
+
+#  Since the app is loaded when django start, @shared_task decorator is able to use it:
+# The @shared_task decorator lets you create tasks without having any concrete app instance:
+@shared_task(name="send_feedback_email_task")
+def send_feedback_email_task(email, message):
+    # sends a feedbak email every 24 hours
+    logger.info("The feedback email was sent succesfully")
+    return send_feedback_email(email, message)
+
